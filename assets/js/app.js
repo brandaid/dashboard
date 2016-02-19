7f09950361c874b2dd1a5e0a2addc3e90a049a34
Eric Paxton <heypaxton@gmail.com>
2016-02-19 15:27:47 -0800

    Add switches

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,25 @@
         return this;
       },
 
-      init: function() {
+      switch: function() {
+        $(".custom-switch").each(function(i) {
+          var classes = $(this).attr("class"),
+              id      = $(this).attr("id"),
+              name    = $(this).attr("name");
 
+          $(this).wrap('<div class="custom-switch" id="' + name + '"></div>');
+        	$(this).after('<label for="custom-switch-' + i + '"></label>');
+          $(this).attr("id", "custom-switch-" + i);
+          $(this).attr("name", name);
+        });
+      },
+
+      init: function() {
+        //vm.switch();
       }
     };
 
     var vm = _dashboard.viewModel();
+    _dashboard.init();
+
 })(jQuery);
