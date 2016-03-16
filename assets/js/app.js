f9c6fac4fbf0050d67809b7e17430f8d5238d356
Eric Paxton <heypaxton@gmail.com>
2016-03-16 02:59:20 -0700

    Add off canvas nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 (function($) {
     $(document).foundation();
-    
+
     var _dashboard = {
       viewModel: function() {
         return this;
@@ -26,10 +26,26 @@
         $('input.datepicker').pickadate();
       },
 
+      toggleNav: function() {
+        var sideNav = $('aside.left');
+        $('.menu-icon').click(function() {
+          if ($('aside.left.active-nav').length < 1) {
+            sideNav.addClass('active-nav');
+            sideNav.animate({"left": "0", "position":"fixed"}, 500);
+            $('.main-content').fadeOut();
+          } else {
+            sideNav.animate({"left": "-500px", "position":"fixed"}, 500);
+            sideNav.removeClass('active-nav');
+            $('.main-content').fadeIn();
+          }
+        });
+      },
+
       init: function() {
         vm.toggleSubNav();
         vm.toggleAvatarDropdown();
         vm.initDatepicker();
+        vm.toggleNav();
       }
     };
 
